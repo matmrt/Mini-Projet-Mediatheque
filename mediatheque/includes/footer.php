1948536d600828e2784ba1ad0c2092b2476dde9a
matmrt <matheomarthely.972@gmail.com>
2026-09-21 16:21:26 +0200

    Delete mediatheque directory

--- mediatheque/includes/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-
-    <footer>
-        <div class="footer-content">
-            <p>&copy; <?= date('Y') ?> — Application Web de Gestion de Médiathèque | BTS TCIEL IR</p>
-        </div>
-    </footer>
-</body>
-</html>
